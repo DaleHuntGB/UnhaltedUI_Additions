1be820edf62fb85c5fceb5ba7290154d6dc6f01b
Dale Hunt <dalehunt@gmail.com>
2024-01-23 11:22:09 +0000

    Update font and properties for UI elements

--- UnhaltedUI_Additions.lua
+++ UnhaltedUI_Additions.lua
@@ -2,6 +2,7 @@ local UnhaltedUI_Additions = CreateFrame("Frame")
 local UH = {}
 local _, class = UnitClass("player")
 local classColor = RAID_CLASS_COLORS[class]
+local UHNormal = "Fonts\\FRIZQT__.TTF"
 UnhaltedUI_Additions:RegisterEvent("PLAYER_LOGIN")
 UnhaltedUI_Additions:RegisterEvent("ADDON_LOADED")
 UnhaltedUI_Additions:SetScript("OnEvent", function(self, event, ...) if event == "PLAYER_LOGIN" then UH:CreateUI() end end)
@@ -71,54 +72,97 @@ function UH:AutoLootPlus()
         end
     end)
 end
+function UH:SetFont(obj, font, size, style, sR, sG, sB, sA, sX, sY, r, g, b, a)
+	if not obj then return end
+	if style == 'NONE' or not style then style = '' end
+	local shadow = strsub(style, 0, 6) == 'SHADOW'
+	if shadow then style = strsub(style, 7) end -- shadow isnt a real style
+	obj:SetFont(font, size, style)
+	obj:SetShadowColor(sR or 0, sG or 0, sB or 0, sA or (shadow and (style == '' and 1 or 0.6)) or 0)
+	obj:SetShadowOffset(sX or (shadow and 1) or 0, sY or (shadow and -1) or 0)
+	if r and g and b then
+		obj:SetTextColor(r, g, b)
+	end
+	if a then
+		obj:SetAlpha(a)
+	end
+end
 function UH:ImproveBlizzardUI()
-    CharacterLevelText:SetFont("Fonts\\FRIZQT__.TTF", 12, "OUTLINE")
-    CharacterLevelText:SetShadowOffset(0, 0)
-    CharacterLevelText:SetShadowColor(0, 0, 0, 0)
-    CharacterFrameTitleText:SetFont("Fonts\\FRIZQT__.TTF", 12, "OUTLINE")
-    CharacterFrameTitleText:SetShadowOffset(0, 0)
-    CharacterFrameTitleText:SetShadowColor(0, 0, 0, 0)
-    ObjectiveTrackerBlocksFrame.QuestHeader.Text:SetFont("Fonts\\FRIZQT__.TTF", 12, "OUTLINE")
-    ObjectiveTrackerBlocksFrame.QuestHeader.Text:SetShadowOffset(0, 0)
-    ObjectiveTrackerBlocksFrame.QuestHeader.Text:SetShadowColor(0, 0, 0, 0)
-    ErrorFont:SetFont("Fonts\\FRIZQT__.TTF", 12, "OUTLINE")
-    ErrorFont:SetShadowOffset(0, 0)
-    ErrorFont:SetShadowColor(0, 0, 0, 0)
-    ZoneTextFont:SetFont("Fonts\\FRIZQT__.TTF", 16, "OUTLINE")
-    ZoneTextFont:SetShadowOffset(0, 0)
-    ZoneTextFont:SetShadowColor(0, 0, 0, 0)
-    SubZoneTextFont:SetFont("Fonts\\FRIZQT__.TTF", 12, "OUTLINE")
-    SubZoneTextFont:SetShadowOffset(0, 0)
-    SubZoneTextFont:SetShadowColor(0, 0, 0, 0)
-    PVPInfoTextFont:SetFont("Fonts\\FRIZQT__.TTF", 16, "OUTLINE")
-    PVPInfoTextFont:SetShadowOffset(0, 0)
-    PVPInfoTextFont:SetShadowColor(0, 0, 0, 0)
-    PVPArenaTextString:SetFont("Fonts\\FRIZQT__.TTF", 12, "OUTLINE")
-    PVPArenaTextString:SetShadowOffset(0, 0)
-    PVPArenaTextString:SetShadowColor(0, 0, 0, 0)
-    ChatBubbleFont:SetFont("Fonts\\FRIZQT__.TTF", 10, "OUTLINE")
-    ChatBubbleFont:SetShadowOffset(0, 0)
-    ChatBubbleFont:SetShadowColor(0, 0, 0, 0)
-    ActionStatus.Text:SetFont("Fonts\\FRIZQT__.TTF", 12, "OUTLINE")
-    ActionStatus.Text:SetShadowOffset(0, 0)
-    ActionStatus.Text:SetShadowColor(0, 0, 0, 0)
-    CharacterStatsPane.ItemLevelCategory.Title:SetFont("Fonts\\FRIZQT__.TTF", 12, "OUTLINE")
-    CharacterStatsPane.ItemLevelCategory.Title:SetShadowOffset(0, 0)
-    CharacterStatsPane.ItemLevelCategory.Title:SetShadowColor(0, 0, 0, 0)
-    CharacterStatsPane.ItemLevelCategory.Title:SetTextColor(classColor.r, classColor.g, classColor.b, 1.0)
-    CharacterStatsPane.ItemLevelFrame.Value:SetAlpha(0)
-    CharacterStatsPane.ItemLevelFrame.Background:SetAlpha(0)
-    CharacterStatsPane.AttributesCategory.Title:SetFont("Fonts\\FRIZQT__.TTF", 12, "OUTLINE")
-    CharacterStatsPane.AttributesCategory.Title:SetShadowOffset(0, 0)
-    CharacterStatsPane.AttributesCategory.Title:SetShadowColor(0, 0, 0, 0)
-    CharacterStatsPane.AttributesCategory.Title:SetTextColor(classColor.r, classColor.g, classColor.b, 1.0)
-    CharacterStatsPane.EnhancementsCategory.Title:SetFont("Fonts\\FRIZQT__.TTF", 12, "OUTLINE")
-    CharacterStatsPane.EnhancementsCategory.Title:SetShadowOffset(0, 0)
-    CharacterStatsPane.EnhancementsCategory.Title:SetShadowColor(0, 0, 0, 0)
-    CharacterStatsPane.EnhancementsCategory.Title:SetTextColor(classColor.r, classColor.g, classColor.b, 1.0)
+-- Set font and properties for CharacterLevelText
+UH:SetFont(CharacterLevelText, UHNormal, 12, "OUTLINE", 0, 0, 0, 0, 0, 0, nil, nil, nil, nil)
+
+-- Set font and properties for CharacterFrameTitleText
+UH:SetFont(CharacterFrameTitleText, UHNormal, 12, "OUTLINE", 0, 0, 0, 0, 0, 0, nil, nil, nil, nil)
+
+-- Set font and properties for ErrorFont
+UH:SetFont(ErrorFont, UHNormal, 14, "OUTLINE", 0, 0, 0, 0, 0, 0, nil, nil, nil, nil)
+
+-- Set font and properties for ZoneTextFont
+UH:SetFont(ZoneTextFont, UHNormal, 24, "OUTLINE", 0, 0, 0, 0, 0, 0, nil, nil, nil, nil)
+
+-- Set font and properties for SubZoneTextFont
+UH:SetFont(SubZoneTextFont, UHNormal, 16, "OUTLINE", 0, 0, 0, 0, 0, 0, nil, nil, nil, nil)
+
+-- Set font and properties for PVPInfoTextFont
+UH:SetFont(PVPInfoTextFont, UHNormal, 24, "OUTLINE", 0, 0, 0, 0, 0, 0, nil, nil, nil, nil)
+
+-- Set font and properties for PVPArenaTextString
+UH:SetFont(PVPArenaTextString, UHNormal, 16, "OUTLINE", 0, 0, 0, 0, 0, 0, nil, nil, nil, nil)
+
+-- Set font and properties for ChatBubbleFont
+UH:SetFont(ChatBubbleFont, UHNormal, 10, "OUTLINE", 0, 0, 0, 0, 0, 0, nil, nil, nil, nil)
+
+-- Set font and properties for ActionStatus Text
+UH:SetFont(ActionStatus.Text, UHNormal, 14, "OUTLINE", 0, 0, 0, 0, 0, 0, nil, nil, nil, nil)
+
+-- Set font and properties for CharacterStatsPane ItemLevelCategory Title
+UH:SetFont(CharacterStatsPane.ItemLevelCategory.Title, UHNormal, 12, "OUTLINE", 0, 0, 0, 0, 0, 0, classColor.r, classColor.g, classColor.b, 1.0)
+
+-- Set alpha transparency for CharacterStatsPane ItemLevelFrame Value and Background
+CharacterStatsPane.ItemLevelFrame.Value:SetAlpha(0)
+CharacterStatsPane.ItemLevelFrame.Background:SetAlpha(0)
+
+-- Set font and properties for CharacterStatsPane AttributesCategory Title
+UH:SetFont(CharacterStatsPane.AttributesCategory.Title, UHNormal, 12, "OUTLINE", 0, 0, 0, 0, 0, 0, classColor.r, classColor.g, classColor.b, 1.0)
+
+-- Set font and properties for CharacterStatsPane EnhancementsCategory Title
+UH:SetFont(CharacterStatsPane.EnhancementsCategory.Title, UHNormal, 12, "OUTLINE", 0, 0, 0, 0, 0, 0, classColor.r, classColor.g, classColor.b, 1.0)
+
+-- ElvUI Font Control
+UH:SetFont(GameFontNormal, UHNormal, 12, "OUTLINE", 0, 0, 0, 0, 0, 0, nil, nil, nil, nil)
+UH:SetFont(GameFontNormalSmall, UHNormal, 12, "OUTLINE", 0, 0, 0, 0, 0, 0, nil, nil, nil, nil)
+UH:SetFont(GameFontNormalLarge, UHNormal, 16, "OUTLINE", 0, 0, 0, 0, 0, 0, nil, nil, nil, nil)
+UH:SetFont(GameFontNormalHuge, UHNormal, 20, "OUTLINE", 0, 0, 0, 0, 0, 0, nil, nil, nil, nil)
+UH:SetFont(QuestFontNormalSmall, UHNormal, 12, "NONE", 0, 0, 0, 0, 0, 0, nil, nil, nil, nil)
+UH:SetFont(QuestFont, UHNormal, 14, "NONE", 0, 0, 0, 0, 0, 0, nil, nil, nil, nil)
+UH:SetFont(QuestTitleFont, UHNormal, 16, "NONE", 0, 0, 0, 0, 0, 0, nil, nil, nil, nil)
+UH:SetFont(GameFontNormalMed2, UHNormal, 14, "OUTLINE", 0, 0, 0, 0, 0, 0, nil, nil, nil, nil)
+UH:SetFont(SystemFont_Huge2, UHNormal, 20, "OUTLINE", 0, 0, 0, 0, 0, 0, nil, nil, nil, nil)
+UH:SetFont(GameFontNormalMed3, UHNormal, 14, "OUTLINE", 0, 0, 0, 0, 0, 0, nil, nil, nil, nil)
+UH:SetFont(BossEmoteNormalHuge, UHNormal, 20, "OUTLINE", 0, 0, 0, 0, 0, 0, nil, nil, nil, nil)
+UH:SetFont(SystemFont16_Shadow_ThickOutline, UHNormal, 16, "OUTLINE", 0, 0, 0, 0, 0, 0, nil, nil, nil, nil)
+
 end
 SLASH_SHOWBARS1 = "/bars"
 SlashCmdList["SHOWBARS"] = function() if C_AddOns.IsAddOnLoaded("Bartender4") then local function t(n) Bartender4.Bar.barregistry[n]:SetVisibilityOption("always",not Bartender4.Bar.barregistry[n]:GetVisibilityOption("always")) end t("1") t("2") t("3") t("4") end end
+
+-- Define a function for the custom slash command
+local function MyPrintCommand(msg)
+    -- Execute the provided Lua code
+    local result = assert(loadstring("return " .. msg))()
+    
+    -- Check if there's any result to print
+    if result then
+        DEFAULT_CHAT_FRAME:AddMessage(tostring(result))
+    end
+end
+
+-- Register the slash command with its function
+SLASH_MYPRINT1 = "/print"
+SlashCmdList["MYPRINT"] = MyPrintCommand
+
+
+
 function UH:CreateUI()
     UH:CreateBackdrops()
     UH:AutoLootPlus()
